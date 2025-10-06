55cd38d587d1bb1abdea67dcd2e468a3e60968f4
Abinaya <abinayapandey360@gmail.com>
2025-10-06 22:38:04 +0545

    Query parameter added
    I) Path parameter II) Query parameter

--- myapi.py
+++ myapi.py
@@ -2,14 +2,7 @@ from fastapi import FastAPI, Path
 
 app = FastAPI()
 
-students = {
-    1:
-    {
-        "name": "John", 
-        "age": 19, 
-        "class": "year 12"
-    }
-}
+students = {1: {"name": "John", "age": 19, "class": "year 12"}}
 
 
 # The decoretors signifies the HTTP method
@@ -19,6 +12,19 @@ def index():  # index() is the function that runs when the endpoint is accessed
 
 
 @app.get("/get-student/{student_id}")  # get-student is an endpoint. just like /home
-def get_student(student_id: int = Path(None, description="The ID of the student you want to view",\
- gt=0, lt=3)):  # student_id is like a variable that stores the value returned
-    return students[student_id]  # its specifying to return student_id / variable from the student
+def get_student(
+    student_id: int = Path(
+        description="The ID of the student you want to view", gt=0, lt=3
+    )
+):  # student_id is like a variable that stores the value returned
+    return students[
+        student_id
+    ]  # its specifying to return student_id / variable from the student
+
+
+@app.get("/get-by-name")
+def get_student(name: str):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not found!"}
